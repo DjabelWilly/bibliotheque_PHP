a1416a668baaf9e9bbb7fc33421eaacb678ca661
nabil_ghazali <nabil.dev.ghaz@hotmail.com>
2024-02-01 10:01:50 +0100

    Ajout visuel

--- src/Controller/GenreController.php
+++ src/Controller/GenreController.php
@@ -18,4 +18,20 @@ class GenreController extends AbstractController
         $this->render('genres', 'Genres', $genreResult);
 
     }
+
+    public function displayLivreByGenre()
+    {
+        $model = Model::getInstance();
+        $livre = $model->getById('livre', $_GET['id']);
+        // récupère la table "genre" en passant par la clé étrangère id_genre présente dans la table "livre"
+        $genre = $model->getById('genre', $livre->getId_genre());
+        $auteur = $model->getByAttribute('livre', ['id' => $livre->getId()]);
+
+        $this->render('genre', $livre->getTitre(), [
+            'livre' => $livre,
+            'auteur' => $auteur,
+            'genre' => $genre->getNom()
+        ]);
+  
+    }
 }
